443c8ab740fb54deb8ac35702d6d84b6495ac781
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-09-04 08:10:29 +0200

    Initial commit.

---
+++ src/Command/ValidateBranchAlias.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * This file is part of the Contao Community Alliance Build System tools.
+ *
+ * @copyright 2014 Contao Community Alliance <https://c-c-a.org>
+ * @author    Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @package   contao-community-alliance/build-system-tool-branch-alias-validation
+ * @license   MIT
+ * @link      https://c-c-a.org
+ */
+namespace ContaoCommunityAlliance\BuildSystem\Tool\BranchAliasValidation\Command;
+
+use ContaoCommunityAlliance\BuildSystem\Repository\GitRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ValidateBranchAlias
+ *
+ * @package ContaoCommunityAlliance\BuildSystem\Tool\BranchAliasValidation\Command
+ */
+class ValidateBranchAlias extends Command
+{
+    /**
+     * The current input interface.
+     *
+     * @var InputInterface
+     */
+    protected $input;
+
+    /**
+     * The current output interface.
+     *
+     * @var OutputInterface
+     */
+    protected $output;
+
+    protected function configure()
+    {
+        $this
+            ->setName('ccabs:tools:validate-branch-alias')
+            ->setDescription('Validate that all branches are ahead of the most recent tag.')
+            ->addArgument(
+                'git-dir',
+                InputArgument::OPTIONAL,
+                'The directory where the git repository is located at.',
+                '.'
+            );
+    }
+
+    protected function simplifyBranch($branch)
+    {
+        return substr($branch, 4);
+    }
+
+    protected function getTagFromBranch($branch)
+    {
+        $git      = new GitRepository($this->input->getArgument('git-dir'));
+        return trim($git->describe()->tags()->execute($branch));
+    }
+
+    /**
+     * Validate the given branch.
+     *
+     * Returns true on success, the name of the offending tag on failure.
+     *
+     * @param  string $tag   The tag to check.
+     *
+     * @param  string $alias The alias for the given branch.
+     *
+     * @return bool|string
+     */
+    public function validate($tag, $alias)
+    {
+        $simpleAlias  = preg_replace("~(\.x)?-dev~", "", $alias);
+        $versionLevel = count(explode('.', $simpleAlias));
+        $reducedTag   = implode('.', array_slice(explode('.', $tag), 0, $versionLevel));
+        return version_compare($reducedTag, $simpleAlias, "<=");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input  = $input;
+        $this->output = $output;
+
+        $composer = json_decode(file_get_contents($input->getArgument('git-dir') . '/composer.json'), true);
+        foreach ($composer["extra"]["branch-alias"] as $branch => $alias) {
+            $tag        = $this->getTagFromBranch($this->simplifyBranch($branch));
+            if (!$this->validate($tag, $alias)) {
+                $output->writeln(
+                    "<error>The branch alias $branch($alias) is behind the latest branch tag $tag!</error>"
+                );
+                return 1;
+            } else {
+                if (OutputInterface::VERBOSITY_VERBOSE <= $output->getVerbosity()) {
+                    $output->writeln(
+                        "<info>Branch alias $branch($alias) is ahead of the latest branch tag.</info>"
+                    );
+                }
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ tests/Command/ValidateBranchAliasTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This file is part of the Contao Community Alliance Build System tools.
+ *
+ * @copyright 2014 Contao Community Alliance <https://c-c-a.org>
+ * @author    Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @package   contao-community-alliance/build-system-tool-branch-alias-validation
+ * @license   MIT
+ * @link      https://c-c-a.org
+ */
+namespace ContaoCommunityAlliance\BuildSystem\Tool\BranchAliasValidation\Test\Command;
+
+use ContaoCommunityAlliance\BuildSystem\Repository\GitRepository;
+use ContaoCommunityAlliance\BuildSystem\Tool\BranchAliasValidation\Command\ValidateBranchAlias;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ValidateBranchAliasTest
+ *
+ * @package ContaoCommunityAlliance\BuildSystem\Tool\BranchAliasValidation\Test\Command
+ */
+class ValidateBranchAliasTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Validate the given branch.
+     *
+     * @return void
+     */
+    public function testValidate()
+    {
+        $command = new ValidateBranchAlias();
+
+        foreach (array
+            (
+                array(
+                    'tag'   => '2.0.0-beta20',
+                    'alias' => '2.0.x-dev',
+                ),
+                array(
+                    'tag'   => '2.0.0-beta20',
+                    'alias' => '3.0.x-dev',
+                ),
+                array(
+                    'tag'   => '2.0.0-beta20',
+                    'alias' => '2.x-dev',
+                ),
+                array(
+                    'tag'   => '2.0.0-beta20',
+                    'alias' => '3.x-dev',
+                ),
+            ) as $entry
+        ) {
+            $this->assertTrue(
+                $command->validate($entry['tag'], $entry['alias']),
+                $entry['tag'] . '<=' . $entry['alias']
+            );
+        }
+
+        foreach (array
+            (
+                array(
+                    'tag'   => '2.0.0-beta20',
+                    'alias' => '1.0.x-dev',
+                ),
+                array(
+                    'tag'   => '3.0.0-beta20',
+                    'alias' => '1.x-dev',
+                ),
+                array(
+                    'tag'   => '2.0.0-beta20',
+                    'alias' => '1.x-dev',
+                ),
+                array(
+                    'tag'   => '4.0.0-beta20',
+                    'alias' => '2.x-dev',
+                ),
+            ) as $entry
+        ) {
+            $this->assertFalse(
+                $command->validate($entry['tag'], $entry['alias']),
+                $entry['tag'] . ' <= ' . $entry['alias']
+            );
+        }
+    }
+}
